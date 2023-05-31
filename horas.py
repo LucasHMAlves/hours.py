d8913b5481833806613d0323e442da418f23eb87
Lucas <130757812+LucasHMAlves@users.noreply.github.com>
2023-05-30 23:01:07 -0300

    Update horas.py

--- horas.py
+++ horas.py
@@ -1,1 +1,20 @@
+import datetime
+import time
 
+while True:
+    # Obtém a data e hora atual
+    agora = datetime.datetime.now()
+
+    # Formata a data e hora como string
+    hora_atual = agora.strftime("%H:%M:%S")
+
+    # Limpa a tela (opcional, dependendo do seu ambiente)
+    # Você pode remover essa linha se não quiser limpar a tela
+    print("\033c", end="")
+
+    # Imprime a hora atual
+    print("Relógio:")
+    print(hora_atual)
+
+    # Aguarda um segundo antes de atualizar o relógio
+    time.sleep(1)
